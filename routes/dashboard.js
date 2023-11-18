fb2df01a57dc16a2c67be7c8fff30573362f1267
Asdrubal J. Fuentes M <asdrubalfuentes@gmail.com>
2023-11-17 23:16:44 -0300

    Agregada Vista de Casa y sus archivos, tambien una ruta de acceso sin implementar un servidor aparte

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
         }
     })*/
     if(req.user.id==='655754169607e1f015508545'){
-        res.redirect('http://emqx.aysafi.com:3001');
+        res.redirect('/inicio/?auth-token=' + req.query['auth-token']);
         console.log('reenviado a su dashboard personal', 'http://emqx.aysafi.com:3001');
     }else if(req.user.id==='6557548c9607e1f015508548' || req.user.id==='655754ec9607e1f01550854b'){
         res.redirect('http://emqx.aysafi.com:3000');
@@ -19,6 +19,11 @@ router.get('/', (req, res) => {
 
 router.get('/users', async(req,res)=>{
     res.render('userlist',{title:"Lista de Usuarios"});
+})
+
+router.get('/inicio', async(req,res)=>{
+    //TODO: AGREGAR REVISION DE CABECERA if()
+    res.render('inicio',{title:"Control de Portones"});
 })
 
 module.exports = router
