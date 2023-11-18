8f5bba0ffc44d179efa07ad89b4282a3239869c3
Asdrubal J. Fuentes M <asdrubalfuentes@gmail.com>
2023-11-17 23:24:36 -0300

    Corregida la ruta

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -9,7 +9,8 @@ router.get('/', (req, res) => {
         }
     })*/
     if(req.user.id==='655754169607e1f015508545'){
-        res.redirect('/inicio/?auth-token=' + req.query['auth-token']);
+        console.log(req.query);
+        res.redirect('/api/dashboard/inicio/?auth-token=' + req.query['auth-token']);
         console.log('reenviado a su dashboard personal', 'http://emqx.aysafi.com:3001');
     }else if(req.user.id==='6557548c9607e1f015508548' || req.user.id==='655754ec9607e1f01550854b'){
         res.redirect('http://emqx.aysafi.com:3000');
