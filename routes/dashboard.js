a4d116f96f9d57b3cdab50d2275e29fae14110b7
Asdrubal J. Fuentes M <asdrubalfuentes@gmail.com>
2023-11-17 23:29:11 -0300

    Estaba mal escrito el nombre de la variable que contenia el token en la query de la pericion GET

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
     })*/
     if(req.user.id==='655754169607e1f015508545'){
         console.log(req.query);
-        res.redirect('/api/dashboard/inicio/?auth-token=' + req.query['auth-token']);
+        res.redirect('/api/dashboard/inicio/?auth-token=' + req.query.auth_token);
         console.log('reenviado a su dashboard personal', 'http://emqx.aysafi.com:3001');
     }else if(req.user.id==='6557548c9607e1f015508548' || req.user.id==='655754ec9607e1f01550854b'){
         res.redirect('http://emqx.aysafi.com:3000');
