c37d7f0a9c5276276158b6edc7846a18c65b9774
Asdrubal J. Fuentes M <asdrubalfuentes@gmail.com>
2023-11-17 23:39:42 -0300

    Todavía no se redirige

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -9,9 +9,9 @@ router.get('/', (req, res) => {
         }
     })*/
     if(req.user.id==='655754169607e1f015508545'){
-        console.log(req.query);
+        //console.log(req.query);
         res.redirect('/api/dashboard/inicio/?auth-token=' + req.query.auth_token);
-        console.log('reenviado a su dashboard personal', 'http://emqx.aysafi.com:3001');
+        console.log('reenviado a su dashboard personal', '/api/dashboard/inicio'); //'http://emqx.aysafi.com:3001'
     }else if(req.user.id==='6557548c9607e1f015508548' || req.user.id==='655754ec9607e1f01550854b'){
         res.redirect('http://emqx.aysafi.com:3000');
         console.log('reenviado a su dashboard personal', 'http://emqx.aysafi.com:3000');
@@ -24,6 +24,7 @@ router.get('/users', async(req,res)=>{
 
 router.get('/inicio', async(req,res)=>{
     //TODO: AGREGAR REVISION DE CABECERA if()
+    console.log(req.query, "Estamos en Inicio");
     res.render('inicio',{title:"Control de Portones"});
 })
 
