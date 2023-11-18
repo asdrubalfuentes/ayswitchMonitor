1e32b5d5dbc47f132a9e7ebb83ef614f57899913
Asdrubal J. Fuentes M <asdrubalfuentes@gmail.com>
2023-11-18 13:28:16 -0300

    Creado y usado el directorio static en la carpeta public del archivo raiz

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyparser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
 app.use(morgan('dev'));
 require('dotenv').config();
 
@@ -40,7 +41,7 @@ const authRoutes = require('./routes/auth');
 const dashboadRoutes = require('./routes/dashboard');
 const verifyToken = require('./routes/validate-token');
 
-app.use('/static', express.static(__dirname + 'views/assets'));
+app.use('/static', express.static(path.join(__dirname + 'public')));
 
 // route middlewares
 app.use('/api/user', authRoutes);
