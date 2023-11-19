44cb6c0ac420aea4bd37e1ccaf484c3d687684c5
Asdrubal J. Fuentes M <asdrubalfuentes@gmail.com>
2023-11-19 15:22:10 -0300

    Creado acceso a otro cliente Don Roberto

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -11,10 +11,10 @@ router.get('/', (req, res) => {
     if(req.user.id==='655754169607e1f015508545'){
         //console.log(req.query);
         res.redirect('/api/dashboard/inicio/?auth_token=' + req.query.auth_token);
-        console.log('reenviado a su dashboard personal', '/api/dashboard/inicio'); //'http://emqx.aysafi.com:3001'
+        //console.log('reenviado a su dashboard personal', '/api/dashboard/inicio'); //'http://emqx.aysafi.com:3001'
     }else if(req.user.id==='6557548c9607e1f015508548' || req.user.id==='655754ec9607e1f01550854b'){
-        res.redirect('http://emqx.aysafi.com:3000');
-        console.log('reenviado a su dashboard personal', 'http://emqx.aysafi.com:3000');
+        res.redirect('/api/dashboard/roberto/?auth_token=' + req.query.auth_token);
+        //console.log('reenviado a su dashboard personal', 'http://emqx.aysafi.com:3000');
     }
 })
 
@@ -23,9 +23,13 @@ router.get('/users', async(req,res)=>{
 })
 
 router.get('/inicio/?', async(req,res)=>{
-    //TODO: AGREGAR REVISION DE CABECERA if()
-    console.log(req.query, "Estamos en Inicio");
+    //console.log(req.query, "Estamos en Inicio");
     res.render('inicio',{title:"Control de Portones", token:req.query.auth_token, user:req.user});
+})
+
+router.get('/roberto/?', async(req,res)=>{
+    //console.log(req.query, "Estamos en Inicio");
+    res.render('roberto',{title:"Control de Portones", token:req.query.auth_token, user:req.user});
 })
 
 module.exports = router
